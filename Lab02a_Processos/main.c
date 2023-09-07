4a3b26f190c01c3c07c8c498d1a322ece4d412f3
Rodrigo Rios <rodrigo.rsrios@gmail.com>
2023-09-07 18:43:10 -0300

    lab 02a

--- Lab02a_Processos/main.c
+++ Lab02a_Processos/main.c
@@ -10,10 +10,9 @@ int main (int argc, char *argv[]) {
         return 1;
     }
     n = atoi(argv[1]);
-    for (i = 1; i < n; i++)
+    for (i = 0; i < n; i++)
         if (childpid = fork())
-            break;
-            
+         break;           
     fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
     return 0; 
 }
